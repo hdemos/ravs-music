1067cb3c338a2a73948fa0d5d0a049dc155ea9db
Haley Demos <haleydemosother@gmail.com>
2020-07-16 14:20:35 -0500

    add color vals

---
+++ src/utils/styles/helpers.js
@@ -0,0 +1,27 @@
+// Helper styles and themes for global usage
+
+export const colors ={
+    red: '#ff5c5c',
+    orange: '#fdac42',
+    yellow: '#fddd48',
+    green: '#39d98a',
+    teal: '#73dee6',
+    blue: '#3e7bfa',
+    darkNavy: '#04264c',
+    purple: '#ac5dd9',
+    white: '#f5f6fd',
+    lightGray: '#9e9e9e',
+    darkGrey: '#54575E',
+
+    shadows: {
+        base: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+
+        light: '0px 0px 11px rgba(45, 45, 45, 0.11)'
+    },
+
+    gradient: {
+        lightPurple: 'linear-gradient(90deg, rgba(78, 113, 251, 1) 0%, rgb(158, 155, 255, 1) 100%)'
+    }
+
+}
+
