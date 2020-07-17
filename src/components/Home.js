b88b166d4b6585caba54cf1695c6761a46b18da4
Haley Demos <haleydemosother@gmail.com>
2020-07-17 14:33:43 -0500

    add album n display correct

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import tangled from "../utils/img/ravs-tangled-album.jpg";
 
 export default class Home extends React.Component {
     render() {
@@ -9,34 +10,37 @@ export default class Home extends React.Component {
                 <div>
                     <h1>Home Page</h1>
                 </div>
-                    <div className="container2">
-                        <div className="bio">
-                            <h2>Bio</h2>
-                            <div className="text">
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                            </div>
-                        </div>
-                        <div className="updates">
-                            <h2>Updates</h2>
-                            <div className="text">
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <div className="container1">
+                        <div className="box-single">
+                            <div className="container2 margin-parent">
+                                <div className="bio margin-child">
+                                    <h2>Bio</h2>
+                                    <div className="text">
+                                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    </div>
+                                </div>
+                                <div className="updates margin-child">
+                                    <h2>Updates</h2>
+                                    <div className="text">
+                                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="container1">
+
+                    <div className="container1 ">
                         <div className="box-single">
                             <h2>Music</h2>
-                            <div className="container2">
-                                <div className="music-square">
-                                    <p>album</p>
-                                    {/*img*/}
+                            <div className="container2 margin-parent">
+                                <div className="music-square margin-child">
+                                    <img className="music-img" src={tangled} label="Musician Ravs first album release: Tangled" alt="warm self portrait with a string of lights tangled in her hair "  />
                                     <div className="hover-btn">
                                     {/*    play btn*/}
                                     </div>
                                 </div>
-                                <div className="music-square">
-                                    <p>album</p>
-                                    {/*img*/}
+                                <div className="music-square margin-child"  >
+                                    <img className="music-img" src={tangled} label="Musician Ravs first album release: Tangled" alt="warm self portrait with a string of lights tangled in her hair "  />
                                     <div className="hover-btn">
                                     {/*    play btn*/}
                                     </div>
